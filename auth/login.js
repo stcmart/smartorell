ae75157b132f7bd4a94d3dc01746c53a723325da
stcmart <b7v9gj2kwp@privaterelay.appleid.com>
2026-09-08 11:10:34 +0000

    Creating auth

---
+++ auth/login.js
@@ -0,0 +1,32 @@
+const SUPABASE_URL = "https://zfhkelulfbnotagrlenl.supabase.co";
+const SUPABASE_KEY = "sb_publishable_TTrqEFYf1bjHlrAJrvo7QQ_qF-8AjIl";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        console.error(error);
+        alert("Correu o contrasenya incorrectes.");
+        return;
+    }
+
+    console.log("Usuari autenticat:", data.user);
+
+    // Aquí pots redirigir a la pàgina principal
+    window.location.href = "index.html";
+});
